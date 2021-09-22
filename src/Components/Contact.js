d52133b1169193c645d018e8ef19095227787373
Stephenfre <stephen.freeman.dev@gmail.com>
2021-09-22 03:43:36 -0700

    fixed mobile styling page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,20 +6,20 @@ export default function Contact() {
         <div className="container">
             <HomeNav />
             <div className="content">
-                <div className="about-me">
+                <div className="contact" style={{ height: "100vh" }}>
                     <h1>Contact</h1>
-                    <h5 style={{ padding: "25px 0" }}>
+                    <h5 className="contact-h5">
                         <strong>Email: </strong>{" "}
                         <a href={"mailto:stephen.freeman.dev@gmail.com"}>stephen.freeman.dev@gmail.com </a>
                     </h5>
-                    <h5 style={{ padding: "25px 0" }}>
+                    <h5 className="contact-h5">
                         <strong>LinkedIn: </strong>{" "}
                         <a href="https://www.linkedin.com/in/stephen-freeman/">linkedin.com/Stephen Freeman</a>
                     </h5>
-                    <h5 style={{ padding: "25px 0" }}>
+                    <h5 className="contact-h5">
                         <strong>Github:</strong> <a href="https://www.github.com/stephenfre">Stephenfre</a>
                     </h5>
-                    <h5 style={{ padding: "25px 0" }}>
+                    <h5 className="contact-h5">
                         <strong>Twitter: </strong> <a href="https://www.twitter.com/devstephenfre">@Devstephenfre</a>
                     </h5>
                 </div>
